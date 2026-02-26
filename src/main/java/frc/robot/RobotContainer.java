b5d1674613befd542e23d70448ab0fe99b58cb39
dabeycorn <Dasun20202020@gmail.com>
2026-02-25 20:05:29 -0500

    replace drive facing limelight target with drive facing pose
    Signed-off-by: dabeycorn <Dasun20202020@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,32 +4,6 @@
 
 package frc.robot;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.math.util.Units;
-import edu.wpi.first.wpilibj.Filesystem;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.SwerveSubsystem;
-import frc.robot.subsystems.ShooterSubsystem_try;
-import frc.robot.subsystems.CANFuelSubsystem;
-import frc.robot.subsystems.VisionSubsystem;
-import frc.robot.util.TunableNumber;
-import swervelib.SwerveDriveTest;
-import swervelib.SwerveModule;
-import swervelib.parser.PIDFConfig;
-import swervelib.parser.SwerveParser;
-
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Meters;
 
@@ -37,27 +11,58 @@ import java.io.File;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 
-public class RobotContainer
-{
-  private final SwerveSubsystem m_drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(),
-                                                                           "swerve"));
-  private final ShooterSubsystem_try m_shooter = new ShooterSubsystem_try();
-  private final CANFuelSubsystem m_fuel = new CANFuelSubsystem();
-  private final VisionSubsystem m_vision = new VisionSubsystem();
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.CANFuelSubsystem;
+import frc.robot.subsystems.ShooterSubsystem_try;
+import frc.robot.subsystems.SwerveSubsystem;
+import frc.robot.subsystems.VisionSubsystem;
+import frc.robot.util.TunableNumber;
+import swervelib.SwerveDriveTest;
+import swervelib.SwerveModule;
+import swervelib.parser.PIDFConfig;
+import swervelib.parser.SwerveParser;
 
+public class RobotContainer {
+  // * CONTROLLERS
   private final CommandXboxController m_driverXbox =
       new CommandXboxController(OperatorConstants.kDriverControllerPort);
   private final CommandXboxController m_secondaryDriverXbox =
       new CommandXboxController(OperatorConstants.kSecondaryDriverControllerPort);
 
-  TunableNumber m_angleP = new TunableNumber("Swerve/PID/ModuleAngle/P", SwerveParser.pidfPropertiesJson.angle.p);
-  TunableNumber m_angleD = new TunableNumber("Swerve/PID/ModuleAngle/D", SwerveParser.pidfPropertiesJson.angle.d);
-  TunableNumber m_driveP = new TunableNumber("Swerve/PID/ModuleDrive/P", SwerveParser.pidfPropertiesJson.drive.p);
-  TunableNumber m_driveD = new TunableNumber("Swerve/PID/ModuleDrive/D", SwerveParser.pidfPropertiesJson.drive.d);
+  // * SUBSYSTEMS
+  private final SwerveSubsystem m_drivebase = new SwerveSubsystem(new File(Filesystem.getDeployDirectory(), "swerve"));
+  private final ShooterSubsystem_try m_shooter = new ShooterSubsystem_try();
+  private final CANFuelSubsystem m_fuel = new CANFuelSubsystem();
+  private final VisionSubsystem m_vision = new VisionSubsystem();
+
+  // * TUNABLES
+  private final TunableNumber m_angleP = new TunableNumber("Swerve/PID/ModuleAngle/P", SwerveParser.pidfPropertiesJson.angle.p);
+  private final TunableNumber m_angleD = new TunableNumber("Swerve/PID/ModuleAngle/D", SwerveParser.pidfPropertiesJson.angle.d);
+  private final TunableNumber m_driveP = new TunableNumber("Swerve/PID/ModuleDrive/P", SwerveParser.pidfPropertiesJson.drive.p);
+  private final TunableNumber m_driveD = new TunableNumber("Swerve/PID/ModuleDrive/D", SwerveParser.pidfPropertiesJson.drive.d);
 
   private SendableChooser<Command> m_autoChooser = null;
 
   public RobotContainer() {
+    SmartDashboard.putData(CommandScheduler.getInstance());
+
+    configureBindings();
+
+    m_autoChooser = AutoBuilder.buildAutoChooser();
+    SmartDashboard.putData("Auto Chooser", m_autoChooser);
+  }
+
+  public void configureBindings() {
     // Field-oriented drive (default)
     Command driveFieldOrientedAnglularVelocity = m_drivebase.driveRobotRelativeCommand(
         () -> MathUtil.applyDeadband(m_driverXbox.getLeftY() * -1, OperatorConstants.LEFT_Y_DEADBAND),
@@ -138,20 +143,23 @@ public class RobotContainer
     // Y = Launch
     m_driverXbox.y().whileTrue(m_fuel.launch());
 
+    // ! since your robot now runs localization, you can assume that your drivetrain produced position is always correct
+    // ! Now, you can just use the handy m_drivebase.driveFacingPose2d(xSupplier, ySupplier, Pose2d); to drive while 
+    // ! facing a position aka target :3
     // RIGHT BUMPER = Auto-aim to target using Limelight
-    m_driverXbox.rightBumper().whileTrue(
-        m_drivebase.run(() -> {
-            if (m_vision.hasTarget()) {
-                double turnSpeed = -m_vision.getTargetX() * 0.03;  // P controller
-                m_drivebase.swerveDrive.drive(
-                    new Translation2d(0, 0),   // No forward/sideways movement
-                    turnSpeed,                  // Rotation speed (rad/s)
-                    true,                       // Field relative
-                    false                       // Not open loop
-                );
-            }
-        })
-    );
+    // m_driverXbox.rightBumper().whileTrue(
+    //     m_drivebase.run(() -> {
+    //         if (m_vision.hasTarget()) {
+    //             double turnSpeed = -m_vision.getTargetX() * 0.03;  // P controller
+    //             m_drivebase.swerveDrive.drive(
+    //                 new Translation2d(0, 0),   // No forward/sideways movement
+    //                 turnSpeed,                  // Rotation speed (rad/s)
+    //                 true,                       // Field relative
+    //                 false                       // Not open loop
+    //             );
+    //         }
+    //     })
+    // );
 
 
     /*
@@ -159,26 +167,9 @@ public class RobotContainer
     addCommandToDashboard(disableArm);
 */
 
-
-   
-
-    SmartDashboard.putData(CommandScheduler.getInstance());
-
-    m_autoChooser = AutoBuilder.buildAutoChooser();
-    SmartDashboard.putData("Auto Chooser", m_autoChooser);
-  }
-
-  private void addCommandToDashboard(Command cmd) {
-    SmartDashboard.putData("cmd/" + cmd.getName(), cmd);
-  }
-
-  public void simulationInit() {
-   
   }
 
   public void robotPeriodic() {
-
-
     SmartDashboard.putNumber("joystick/left-X", m_driverXbox.getLeftX());
     SmartDashboard.putNumber("joystick/left-Y", m_driverXbox.getLeftY());
     SmartDashboard.putNumber("joystick/right-X", m_driverXbox.getRightX());
@@ -200,20 +191,23 @@ public class RobotContainer
         module.getDriveMotor().configurePIDF(new PIDFConfig(m_driveP.get(), m_driveD.get()));
       }
     }
+
+    // ! Send vision position estimates to drivebase odo
+    var estimates = m_vision.getPoseEstimates(m_drivebase.getHeading());
+    for (var estimate : estimates) {
+      m_drivebase.addVisionEstimate(estimate.pose, estimate.timestampSeconds);
+    }
+  }
+
+
+  private void addCommandToDashboard(Command cmd) {
+    SmartDashboard.putData("cmd/" + cmd.getName(), cmd);
   }
 
   public Command getAutonomousCommand() {
     // return new PathPlannerAuto("test auto");
      return m_autoChooser.getSelected();
-    // return drivebase.getAutonomousCommand("small path");
-
-    // An example command will be run in autonomous
-    // return Autos.exampleAuto(m_exampleSubsystem);
-        
-    /*return new SequentialCommandGroup(
-      m_drivebase.driveAtSpeed(5, 0, 0, false).withTimeout(1.2)
-     // drivebase.driveAtSpeed(-5, 0, 0, false).withTimeout(0.5) );*/
- }
+  }
 
   public void setMotorBrake(boolean brake) {
     m_drivebase.setMotorBrake(brake);
